f7c0bbd7ad5fea24de99f5d4302155c39e5b36c4
Ravni Arador <ravarador@gmail.com>
2020-11-30 18:15:55 +0800

    Added DatabaseContext with DbSets Services, Summaries, Transactions, Vehicles

---
+++ BoozewasherApp/BoozewasherApp/Context/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using BoozewasherApp.Models.ContextModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoozewasherApp.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base(@"Data Source=localhost\SQLEXPRESS; Initial Catalog=BoozewasherAppDb;Trusted_Connection=True;")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Summary> Summaries { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}

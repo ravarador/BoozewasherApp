a36506e8bf6f31484cc77106437491100d932d83
Ravni Arador <ravarador@gmail.com>
2020-12-03 10:02:58 +0800

    Fixed minor issue in ServicesController, Added SeedServicesTable migration

--- BoozewasherApp/BoozewasherApp-Web/Controllers/API/ServicesController.cs
+++ BoozewasherApp/BoozewasherApp-Web/Controllers/API/ServicesController.cs
@@ -29,7 +29,7 @@ namespace BoozewasherApp_Web.Controllers.API
         } 
 
         //GET /API/Services/1
-        public IHttpActionResult GetService (int id)
+        public IHttpActionResult GetService(int id)
         {
             var service = _context.Services.SingleOrDefault(s => s.Id == id);
 
@@ -57,7 +57,7 @@ namespace BoozewasherApp_Web.Controllers.API
 
         //PUT /API/Services/1
         [HttpPut]
-        public void UpdateService(int id, ServiceDto serviceDto)
+        public void UpdateService(ServiceDto serviceDto)
         {
             if (!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);

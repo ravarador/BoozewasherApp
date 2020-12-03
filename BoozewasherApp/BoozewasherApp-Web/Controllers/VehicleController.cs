eca10b17161cb0cfe278280185b37f7ab3d01029
DeniseVillacorta <villacorta90806denise@gmail.com>
2020-12-03 12:12:55 +0800

    Added Vehicle Controller and ViewModel

---
+++ BoozewasherApp/BoozewasherApp-Web/Controllers/VehicleController.cs
@@ -0,0 +1,49 @@
+﻿using BoozewasherApp_Web.Models;
+using BoozewasherApp_Web.View_Models.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoozewasherApp_Web.Controllers
+{
+    public class VehicleController : Controller
+    {
+        private ApplicationDbContext _context;
+        public VehicleController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+        }
+        public ActionResult Index()
+        {
+            var vehicles = _context.Vehicles.ToList();
+            return View(vehicles);
+        }
+
+        public ActionResult New()
+        {
+            return View("VehicleForm");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var vehicle = _context.Vehicles.SingleOrDefault(v => v.Id == id);
+
+            if (vehicle == null)
+                return HttpNotFound();
+            var viewModel = new VehicleFormViewModel(vehicle)
+            {
+
+            };
+            return View("VehicleForm", viewModel);
+        }
+
+    }
+}
---
+++ BoozewasherApp/BoozewasherApp-Web/View_Models/Vehicles/VehicleFormViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BoozewasherApp_Web.Models.ContextModel;
+
+namespace BoozewasherApp_Web.View_Models.Vehicles
+{
+    public class VehicleFormViewModel
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public VehicleFormViewModel()
+        {
+            Id = 0;
+        }
+        public VehicleFormViewModel(Vehicle vehicle)
+        {
+            Type = vehicle.Type;
+            Description = vehicle.Description;
+            Brand = vehicle.Brand;
+            Model = vehicle.Model;
+        }
+    }
+}

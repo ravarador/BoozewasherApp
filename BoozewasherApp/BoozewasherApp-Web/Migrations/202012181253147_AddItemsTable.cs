13efd74bb0c373ee63a1b22fcb5a670f05c7c49a
Ravni Arador <ravarador@gmail.com>
2020-12-18 20:53:46 +0800

    Added migration AddItemsTable

---
+++ BoozewasherApp/BoozewasherApp-Web/Migrations/202012181253147_AddItemsTable.cs
@@ -0,0 +1,31 @@
+﻿namespace BoozewasherApp_Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddItemsTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Items",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                        Barcode = c.String(),
+                        IsEmpty = c.Boolean(nullable: false),
+                        UsageCount = c.Int(nullable: false),
+                        Expense = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Items");
+        }
+    }
+}

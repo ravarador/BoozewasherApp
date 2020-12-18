6c83388399e85550e91efbf33cee510d34bd76ae
Ravni Arador <ravarador@gmail.com>
2020-12-18 21:10:50 +0800

    Added migration AddItemsListToTransaction

---
+++ BoozewasherApp/BoozewasherApp-Web/Migrations/202012181309432_AddItemsListToTransaction.cs
@@ -0,0 +1,22 @@
+﻿namespace BoozewasherApp_Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddItemsListToTransaction : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Items", "Transaction_Id", c => c.Int());
+            CreateIndex("dbo.Items", "Transaction_Id");
+            AddForeignKey("dbo.Items", "Transaction_Id", "dbo.Transactions", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Items", "Transaction_Id", "dbo.Transactions");
+            DropIndex("dbo.Items", new[] { "Transaction_Id" });
+            DropColumn("dbo.Items", "Transaction_Id");
+        }
+    }
+}

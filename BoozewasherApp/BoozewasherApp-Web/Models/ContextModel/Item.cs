f0f3e6139361628897c12452f17a6649beeaf0c2
DeniseVillacorta <villacorta9806denise@gmail.com>
2020-12-21 20:16:00 +0800

    Added Data Annotations for Item Table

--- BoozewasherApp/BoozewasherApp-Web/Models/ContextModel/Item.cs
+++ BoozewasherApp/BoozewasherApp-Web/Models/ContextModel/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,20 @@ namespace BoozewasherApp_Web.Models.ContextModel
     public class Item
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Item Barcode")]
         public string Barcode { get; set; }
         public bool IsEmpty { get; set; }
+
+        [Required]
+        [Display (Name = "Usage Count")]
         public int UsageCount { get; set; }
+        [Required]
         public decimal Expense { get; set; }
     }
 }
--- BoozewasherApp/BoozewasherApp-Web/Models/ViewModels/Items/ItemViewModel.cs
+++ BoozewasherApp/BoozewasherApp-Web/Models/ViewModels/Items/ItemViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,18 @@ namespace BoozewasherApp_Web.Models.ViewModels.Items
     public class ItemViewModel
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [Required]
         public string Barcode { get; set; }
         public bool IsEmpty { get; set; }
+
+        [Required]
         public int UsageCount { get; set; }
+        [Required]
         public decimal Expense { get; set; }
     }
 }

42531e157bf26c07099b2378f23a60f0b71d6c7f
Ravni Arador <ravarador@gmail.com>
2020-12-21 09:18:03 +0800

    Added ItemRepository

---
+++ BoozewasherApp/BoozewasherInfrastructure/Repositories/ItemRepository.cs
@@ -0,0 +1,61 @@
+﻿using BoozewasherDomain.Entities;
+using BoozewasherInfrastructure.Properties;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoozewasherInfrastructure.Repositories
+{
+    public class ItemRepository
+    {
+        public void AddItem(Item item)
+        {
+            var client = new RestClient(Resources.ConnectionString);
+            var request = new RestRequest("/api/services/createitem/", Method.POST);
+            request.AddJsonBody(item);
+            request.RequestFormat = DataFormat.Json;
+            var response = client.Execute(request);
+        }
+
+        public void DeleteItem(int id)
+        {
+            var client = new RestClient(Resources.ConnectionString);
+            var request = new RestRequest("/api/items/deleteitem/" + id, Method.DELETE);
+            request.RequestFormat = DataFormat.Json;
+            var response = client.Execute(request);
+        }
+
+        public List<Item> GetAllItems()
+        {
+            var client = new RestClient(Resources.ConnectionString);
+            var request = new RestRequest("/api/items/getitems/", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            var response = client.Execute(request);
+
+            var dataList = JsonConvert.DeserializeObject<List<Item>>(response.Content);
+
+            return dataList;
+        }
+
+        //public Service GetServiceById(int id)
+        //{
+        //    var serviceById = context.Services.Where(a => a.Id == id).FirstOrDefault();
+
+        //    return serviceById;
+        //}
+
+        public void UpdateItem(Item item)
+        {
+            var client = new RestClient(Resources.ConnectionString);
+            var request = new RestRequest("/api/items/updateitem/" + item.Id, Method.PUT);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(item);
+            var response = client.Execute(request);
+
+        }
+    }
+}

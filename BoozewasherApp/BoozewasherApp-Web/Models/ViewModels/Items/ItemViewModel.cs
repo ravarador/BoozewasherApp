6588210707694154df0dd9f837ae06d679ca5ec7
DeniseVillacorta <villacorta9806denise@gmail.com>
2020-12-21 20:08:38 +0800

    Added Item Controller and ViewModel

---
+++ BoozewasherApp/BoozewasherApp-Web/Models/ViewModels/Items/ItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoozewasherApp_Web.Models.ViewModels.Items
+{
+    public class ItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Barcode { get; set; }
+        public bool IsEmpty { get; set; }
+        public int UsageCount { get; set; }
+        public decimal Expense { get; set; }
+    }
+}
---
+++ BoozewasherApp/BoozewasherApp-Web/Models/ViewModels/Items/ItemsFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BoozewasherApp_Web.Models.ContextModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoozewasherApp_Web.Models.ViewModels.Items
+{
+    public class ItemsFormViewModel
+    {
+        public Item Items { get; set; }
+    }
+}

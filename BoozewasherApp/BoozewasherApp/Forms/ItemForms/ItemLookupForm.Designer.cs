9dbb4b558e78bee332ca8e86b1ad1d5af498ee50
Ravni Arador <ravarador@gmail.com>
2020-12-26 16:44:06 +0800

    Added ItemLookupForm

---
+++ BoozewasherApp/BoozewasherApp/Forms/ItemForms/ItemLookupForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace BoozewasherApp.Forms.ItemForms
+{
+    partial class ItemLookupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvItem = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvItem
+            // 
+            this.dgvItem.AllowUserToAddRows = false;
+            this.dgvItem.AllowUserToDeleteRows = false;
+            this.dgvItem.AllowUserToResizeColumns = false;
+            this.dgvItem.AllowUserToResizeRows = false;
+            this.dgvItem.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvItem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvItem.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvItem.Location = new System.Drawing.Point(0, 83);
+            this.dgvItem.Name = "dgvItem";
+            this.dgvItem.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvItem.Size = new System.Drawing.Size(541, 535);
+            this.dgvItem.TabIndex = 1;
+            this.dgvItem.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvItem_CellDoubleClick);
+            // 
+            // ItemLookupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 618);
+            this.Controls.Add(this.dgvItem);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ItemLookupForm";
+            this.Text = "ItemLookupForm";
+            this.Load += new System.EventHandler(this.ItemLookupForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItem)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvItem;
+    }
+}
---
+++ BoozewasherApp/BoozewasherApp/Forms/ItemForms/ItemLookupForm.cs
@@ -0,0 +1,43 @@
+﻿using BoozewasherDomain.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoozewasherApp.Forms.ItemForms
+{
+    public partial class ItemLookupForm : Form
+    {
+        private IItemRepository ItemRepository { get; set; }
+        private int SelectedItemId { get; set; }
+        public ItemLookupForm(IItemRepository itemRepository)
+        {
+            InitializeComponent();
+            ItemRepository = itemRepository;
+        }
+
+        private void ItemLookupForm_Load(object sender, EventArgs e)
+        {
+            LoadDgvItem();
+        }
+
+        private void dgvItem_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            SelectedItemId = (int)dgvItem.SelectedRows[0].Cells[0].Value;
+
+            this.Close();
+        }
+
+        #region Private Methods
+        private void LoadDgvItem()
+        {
+            dgvItem.DataSource = ItemRepository.GetAllItems();
+        }
+        #endregion
+    }
+}

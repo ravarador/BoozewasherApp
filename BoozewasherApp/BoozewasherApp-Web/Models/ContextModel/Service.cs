a59eb37f37a018802319c648e13ea1ff317c1451
DeniseVillacorta <villacorta9806denise@gmail.com>
2021-01-07 21:32:29 +0800

    Fixed edit service issue

--- BoozewasherApp/BoozewasherApp-Web/Models/ContextModel/Service.cs
+++ BoozewasherApp/BoozewasherApp-Web/Models/ContextModel/Service.cs
@@ -9,6 +9,7 @@ namespace BoozewasherApp_Web.Models.ContextModel
     public class Service
     {
         public int Id { get; set; }
+
         [Required]
         [Display(Name = "Service Type")]
         public string Type { get; set; }

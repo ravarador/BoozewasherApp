695a4ce09dc0eef66fc49ea911d70d0f2bc7ae30
Ravni Arador <ravarador@gmail.com>
2021-01-15 20:08:47 +0800

    Added ItemValidator

--- BoozewasherApp/BoozewasherApp/Forms/ItemForms/ItemValidator.cs
+++ BoozewasherApp/BoozewasherApp/Forms/ItemForms/ItemValidator.cs
@@ -4,10 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BoozewasherDomain.Entities;
+using FluentValidation;
 
 namespace BoozewasherApp.Forms.ItemForms
 {
     public class ItemValidator : BaseAbstractValidator<Item>
     {
+        public ItemValidator(Item item)
+        {
+            RuleFor(v => v.Name)
+                .NotEmpty().When(m => string.IsNullOrWhiteSpace(m.Name))
+                .WithMessage("Name cannot be blank or whitespace!");
+
+            ValidationResult = Validate(item);
+        }
     }
 }

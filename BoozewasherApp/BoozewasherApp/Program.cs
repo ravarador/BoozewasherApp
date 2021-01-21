14be4bf161b5656818e69170d9fc94f28fe829e3
Ravni Arador <ravarador@gmail.com>
2021-01-21 11:39:33 +0800

    Added registration of container for Branch repository and irepository

--- BoozewasherApp/BoozewasherApp/Program.cs
+++ BoozewasherApp/BoozewasherApp/Program.cs
@@ -27,6 +27,7 @@ namespace BoozewasherApp
             container.Register<ITransactionRepository, TransactionRepository>(Lifestyle.Singleton);
             container.Register<IItemRepository, ItemRepository>(Lifestyle.Singleton);
             container.Register<ILoginRepository, LoginRepository>(Lifestyle.Singleton);
+            container.Register<IBranchRepository, BranchRepository>(Lifestyle.Singleton);
             //container.Register<ParentForm>(Lifestyle.Singleton);
             //container.Register<MainForm>(Lifestyle.Singleton);
             container.Register<LoginForm>(Lifestyle.Singleton);

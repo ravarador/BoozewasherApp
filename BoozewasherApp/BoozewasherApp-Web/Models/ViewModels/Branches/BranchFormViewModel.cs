861b626b7c8653004cd13f9e5f5be8a637485efd
Denise Villacorta <villacorta9806denise@gmail.com>
2021-01-25 21:27:15 +0800

    Added BranchViewModel and BranchFormViewModel

---
+++ BoozewasherApp/BoozewasherApp-Web/Models/ViewModels/Branches/BranchFormViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BoozewasherApp_Web.Models.ContextModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoozewasherApp_Web.Models.ViewModels.Branches
+{
+    public class BranchFormViewModel
+    {
+        public Branch Branch { get; set; }
+        public string Title
+        {
+            get
+            {
+                if (Branch != null && Branch.Id != 0)
+                    return "Edit Branch";
+
+                return "New Branch";
+            }
+        }
+    }
+}
---
+++ BoozewasherApp/BoozewasherApp-Web/Models/ViewModels/Branches/BranchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoozewasherApp_Web.Models.ViewModels.Branches
+{
+    public class BranchViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}

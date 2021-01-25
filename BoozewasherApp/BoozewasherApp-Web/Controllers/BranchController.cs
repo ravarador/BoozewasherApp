dc23aff8fd9efab2d929b084ff428a1ab0f17a2d
Denise Villacorta <villacorta9806denise@gmail.com>
2021-01-25 21:15:15 +0800

    Added BranchController

---
+++ BoozewasherApp/BoozewasherApp-Web/Controllers/BranchController.cs
@@ -0,0 +1,31 @@
+﻿using BoozewasherApp_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoozewasherApp_Web.Controllers
+{
+    public class BranchController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public BranchController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Branch
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

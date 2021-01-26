2b5b3dc18b60ce2dbb22d8f39cedf928b55c054a
Ravni Arador <ravarador@gmail.com>
2021-01-26 23:18:27 +0800

    Added EmployeeRepository and Employee model in App

---
+++ BoozewasherApp/BoozewasherDomain/Entities/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoozewasherDomain.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Birthdate { get; set; }
+        public string TIN { get; set; }
+        public string Email { get; set; }
+        public string ContactNo { get; set; }
+        public Branch Branch { get; set; }
+        public int BranchId { get; set; }
+    }
+}
---
+++ BoozewasherApp/BoozewasherInfrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,78 @@
+﻿using BoozewasherDomain.Entities;
+using BoozewasherInfrastructure.Properties;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoozewasherInfrastructure.Repositories
+{
+    public class EmployeeRepository
+    {
+        public void AddEmployee(Employee employee)
+        {
+            var client = new RestClient(Resources.ConnectionString);
+            var request = new RestRequest("/api/employees/createemployee/", Method.POST);
+            request.AddJsonBody(employee);
+            request.RequestFormat = DataFormat.Json;
+            var response = client.Execute(request);
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            var client = new RestClient(Resources.ConnectionString);
+            var request = new RestRequest("/api/employees/deleteemployee/" + id, Method.DELETE);
+            request.RequestFormat = DataFormat.Json;
+            var response = client.Execute(request);
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            var client = new RestClient(Resources.ConnectionString);
+            var request = new RestRequest("/api/employees/getemployees/", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            var response = client.Execute(request);
+
+            var dataList = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+
+            return dataList.ToList();
+        }
+
+        public List<Employee> GetEmployeesByBranchId(int id)
+        {
+            var client = new RestClient(Resources.ConnectionString);
+            var request = new RestRequest("/api/employees/getemployeesbybranchid/" + id, Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            var response = client.Execute(request);
+
+            var dataList = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+
+            return dataList.ToList();
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            var client = new RestClient(Resources.ConnectionString);
+            var request = new RestRequest("/api/employees/getemployee/" + id, Method.GET);
+
+            var response = client.Execute(request);
+
+            var data = JsonConvert.DeserializeObject<Employee>(response.Content);
+
+            return data;
+        }
+
+        public void UpdateItem(Employee employee)
+        {
+            var client = new RestClient(Resources.ConnectionString);
+            var request = new RestRequest("/api/employees/updateemployee/" + employee.Id, Method.PUT);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(employee);
+            var response = client.Execute(request);
+
+        }
+    }
+}

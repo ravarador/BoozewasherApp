cae577d50c21026c412d0cde9c20de3d5fc06a71
Ravni Arador <ravarador@gmail.com>
2021-01-26 23:20:49 +0800

    Added IEmployeeRepository

---
+++ BoozewasherApp/BoozewasherDomain/IRepositories/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using BoozewasherDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoozewasherDomain.IRepositories
+{
+    public interface IEmployeeRepository
+    {
+        void AddEmployee(Employee employee);
+        void DeleteEmployee(int id);
+        List<Employee> GetAllEmployees();
+        List<Employee> GetEmployeesByBranchId(int id);
+        Employee GetEmployeeById(int id);
+        void UpdateItem(Employee employee);
+    }
+}

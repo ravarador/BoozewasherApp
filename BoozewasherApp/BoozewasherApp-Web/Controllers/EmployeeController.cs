8bae8f27a05d0192513403b472006e776ee4d27a
Denise Villacorta <villacorta9806denise@gmail.com>
2021-01-27 17:43:15 +0800

    Added EmployeeController

---
+++ BoozewasherApp/BoozewasherApp-Web/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoozewasherApp_Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}

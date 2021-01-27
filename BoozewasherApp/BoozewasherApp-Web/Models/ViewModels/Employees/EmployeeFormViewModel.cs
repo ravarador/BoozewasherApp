72a13cd58f751028bab36cbda26c69a64e8c4c5d
Denise Villacorta <villacorta9806denise@gmail.com>
2021-01-27 18:20:07 +0800

    Added EmployeeViewModel and EmployeeFormViewModel

---
+++ BoozewasherApp/BoozewasherApp-Web/Models/ViewModels/Employees/EmployeeFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BoozewasherApp_Web.Models.ContextModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoozewasherApp_Web.Models.ViewModels.Employees
+{
+    public class EmployeeFormViewModel
+    {
+        public Employee Employee { get; set; }
+    }
+}
---
+++ BoozewasherApp/BoozewasherApp-Web/Models/ViewModels/Employees/EmployeeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BoozewasherApp_Web.Models.ContextModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoozewasherApp_Web.Models.ViewModels.Employees
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Birthdate { get; set; }
+        public string TIN { get; set; }
+        public string Email { get; set; }
+        public string ContactNo { get; set; }
+        public Branch Branch { get; set; }
+        public int BranchId { get; set; }
+    }
+}

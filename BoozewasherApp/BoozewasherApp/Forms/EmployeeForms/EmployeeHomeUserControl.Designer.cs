1b54d63029b251ad5b5f11ee12c060220fa07a35
Ravni Arador <ravarador@gmail.com>
2021-01-29 12:55:25 +0800

    added EmployeeHomeUserControl (work in progress UI)

--- BoozewasherApp/BoozewasherApp/Forms/EmployeeForms/EmployeeHomeUserControl.Designer.cs
+++ BoozewasherApp/BoozewasherApp/Forms/EmployeeForms/EmployeeHomeUserControl.Designer.cs
@@ -29,18 +29,177 @@ namespace BoozewasherApp.Forms.EmployeeForms
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 47);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Total Employees";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(26, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(228, 112);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(260, 18);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(228, 112);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(728, 18);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(228, 112);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Location = new System.Drawing.Point(494, 18);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(228, 112);
+            this.panel4.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 47);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Employees Present";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 47);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Employees Late";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 47);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Employees Absent";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Gadugi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(728, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 93);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "View Attendance";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Gadugi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(728, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 93);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Time In / Time Out";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(26, 136);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(696, 383);
+            this.flowLayoutPanel1.TabIndex = 9;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Location = new System.Drawing.Point(728, 136);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(228, 185);
+            this.panel5.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 47);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Employees Absent";
             // 
             // EmployeeHomeUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "EmployeeHomeUserControl";
             this.Size = new System.Drawing.Size(983, 542);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label5;
     }
 }

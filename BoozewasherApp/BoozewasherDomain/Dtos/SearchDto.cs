446b20428865c27a3dcf58d1ba470bb8a8438563
Ravni Arador <ravarador@gmail.com>
2021-01-31 20:24:26 +0800

    Added SearchDto in winApp

---
+++ BoozewasherApp/BoozewasherDomain/Dtos/SearchDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoozewasherDomain.Dtos
+{
+    public class SearchDto
+    {
+        public string SearchBy { get; set; }
+        public string SearchText { get; set; }
+        public int BranchId { get; set; }
+    }
+}

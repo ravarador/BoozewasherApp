5b5a877f3d97e6aa986dc32c58f6966bf93ba41f
Ravni Arador <ravarador@gmail.com>
2021-01-31 20:28:51 +0800

    Added SearchDto in API

---
+++ BoozewasherApp/BoozewasherApp-Web/Models/Dtos/SearchDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoozewasherApp_Web.Models.Dtos
+{
+    public class SearchDto
+    {
+        public string SearchBy { get; set; }
+        public string SearchText { get; set; }
+        public int BranchId { get; set; }
+    }
+}
+}

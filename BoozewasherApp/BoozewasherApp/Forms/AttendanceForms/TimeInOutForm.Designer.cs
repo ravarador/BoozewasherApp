13eb56fa48668d315a3eec52a27d0ce08c687fa5
Ravni Arador <ravarador@gmail.com>
2021-02-08 21:36:58 +0800

    Added TimeInOutForm

---
+++ BoozewasherApp/BoozewasherApp/Forms/AttendanceForms/TimeInOutForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace BoozewasherApp.Forms.AttendanceForms
+{
+    partial class TimeInOutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboEmployees = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTimeIn = new System.Windows.Forms.Button();
+            this.btnTimeOut = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboEmployees
+            // 
+            this.comboEmployees.FormattingEnabled = true;
+            this.comboEmployees.Location = new System.Drawing.Point(150, 70);
+            this.comboEmployees.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboEmployees.Name = "comboEmployees";
+            this.comboEmployees.Size = new System.Drawing.Size(215, 24);
+            this.comboEmployees.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Employee";
+            // 
+            // btnTimeIn
+            // 
+            this.btnTimeIn.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnTimeIn.FlatAppearance.BorderSize = 0;
+            this.btnTimeIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTimeIn.Location = new System.Drawing.Point(47, 129);
+            this.btnTimeIn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnTimeIn.Name = "btnTimeIn";
+            this.btnTimeIn.Size = new System.Drawing.Size(156, 46);
+            this.btnTimeIn.TabIndex = 2;
+            this.btnTimeIn.Text = "Time In";
+            this.btnTimeIn.UseVisualStyleBackColor = false;
+            // 
+            // btnTimeOut
+            // 
+            this.btnTimeOut.BackColor = System.Drawing.Color.DarkRed;
+            this.btnTimeOut.FlatAppearance.BorderSize = 0;
+            this.btnTimeOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTimeOut.ForeColor = System.Drawing.Color.White;
+            this.btnTimeOut.Location = new System.Drawing.Point(210, 129);
+            this.btnTimeOut.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnTimeOut.Name = "btnTimeOut";
+            this.btnTimeOut.Size = new System.Drawing.Size(156, 46);
+            this.btnTimeOut.TabIndex = 3;
+            this.btnTimeOut.Text = "Time Out";
+            this.btnTimeOut.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(419, 35);
+            this.panel1.TabIndex = 7;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackgroundImage = global::BoozewasherApp.Properties.Resources.close_white_32x32;
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Location = new System.Drawing.Point(386, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(33, 39);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Time In / Time Out";
+            // 
+            // TimeInOutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(419, 218);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnTimeOut);
+            this.Controls.Add(this.btnTimeIn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboEmployees);
+            this.Font = new System.Drawing.Font("Gadugi", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "TimeInOutForm";
+            this.Text = "TimeInOutForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboEmployees;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTimeIn;
+        private System.Windows.Forms.Button btnTimeOut;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label3;
+    }
+}

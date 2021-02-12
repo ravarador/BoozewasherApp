d940fa074bc412d42c356bcf1a9fef1aaffd47fb
Ravni Arador <ravarador@gmail.com>
2021-02-12 15:44:58 +0800

    Fixed UI EmployeeHomeUserControl

--- BoozewasherApp/BoozewasherApp/Forms/EmployeeForms/EmployeeHomeUserControl.Designer.cs
+++ BoozewasherApp/BoozewasherApp/Forms/EmployeeForms/EmployeeHomeUserControl.Designer.cs
@@ -49,6 +49,10 @@ namespace BoozewasherApp.Forms.EmployeeForms
             this.label5 = new System.Windows.Forms.Label();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
@@ -75,6 +79,7 @@ namespace BoozewasherApp.Forms.EmployeeForms
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(22, 18);
@@ -85,9 +90,9 @@ namespace BoozewasherApp.Forms.EmployeeForms
             // pictureBox1
             // 
             this.pictureBox1.Image = global::BoozewasherApp.Properties.Resources.supervisor_account_black_192x192;
-            this.pictureBox1.Location = new System.Drawing.Point(119, 8);
+            this.pictureBox1.Location = new System.Drawing.Point(154, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 68);
+            this.pictureBox1.Size = new System.Drawing.Size(65, 55);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 11;
             this.pictureBox1.TabStop = false;
@@ -95,6 +100,7 @@ namespace BoozewasherApp.Forms.EmployeeForms
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label7);
             this.panel2.Controls.Add(this.pictureBox2);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Location = new System.Drawing.Point(261, 18);
@@ -105,9 +111,9 @@ namespace BoozewasherApp.Forms.EmployeeForms
             // pictureBox2
             // 
             this.pictureBox2.Image = global::BoozewasherApp.Properties.Resources._51209;
-            this.pictureBox2.Location = new System.Drawing.Point(120, 8);
+            this.pictureBox2.Location = new System.Drawing.Point(145, 8);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 68);
+            this.pictureBox2.Size = new System.Drawing.Size(75, 50);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 12;
             this.pictureBox2.TabStop = false;
@@ -124,6 +130,7 @@ namespace BoozewasherApp.Forms.EmployeeForms
             // panel3
             // 
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.label9);
             this.panel3.Controls.Add(this.pictureBox4);
             this.panel3.Controls.Add(this.label4);
             this.panel3.Location = new System.Drawing.Point(734, 18);
@@ -134,9 +141,9 @@ namespace BoozewasherApp.Forms.EmployeeForms
             // pictureBox4
             // 
             this.pictureBox4.Image = global::BoozewasherApp.Properties.Resources._51418;
-            this.pictureBox4.Location = new System.Drawing.Point(119, 8);
+            this.pictureBox4.Location = new System.Drawing.Point(151, 8);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(100, 68);
+            this.pictureBox4.Size = new System.Drawing.Size(68, 50);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 12;
             this.pictureBox4.TabStop = false;
@@ -153,6 +160,7 @@ namespace BoozewasherApp.Forms.EmployeeForms
             // panel4
             // 
             this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label8);
             this.panel4.Controls.Add(this.pictureBox3);
             this.panel4.Controls.Add(this.label3);
             this.panel4.Location = new System.Drawing.Point(500, 18);
@@ -163,9 +171,9 @@ namespace BoozewasherApp.Forms.EmployeeForms
             // pictureBox3
             // 
             this.pictureBox3.Image = global::BoozewasherApp.Properties.Resources.img_454415;
-            this.pictureBox3.Location = new System.Drawing.Point(120, 8);
+            this.pictureBox3.Location = new System.Drawing.Point(145, 8);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(100, 68);
+            this.pictureBox3.Size = new System.Drawing.Size(75, 50);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 12;
             this.pictureBox3.TabStop = false;
@@ -209,9 +217,9 @@ namespace BoozewasherApp.Forms.EmployeeForms
             // 
             this.presentEmployeesFlowLayoutPanel.AutoScroll = true;
             this.presentEmployeesFlowLayoutPanel.BackColor = System.Drawing.Color.Gray;
-            this.presentEmployeesFlowLayoutPanel.Location = new System.Drawing.Point(22, 239);
+            this.presentEmployeesFlowLayoutPanel.Location = new System.Drawing.Point(22, 136);
             this.presentEmployeesFlowLayoutPanel.Name = "presentEmployeesFlowLayoutPanel";
-            this.presentEmployeesFlowLayoutPanel.Size = new System.Drawing.Size(706, 280);
+            this.presentEmployeesFlowLayoutPanel.Size = new System.Drawing.Size(706, 383);
             this.presentEmployeesFlowLayoutPanel.TabIndex = 9;
             this.presentEmployeesFlowLayoutPanel.WrapContents = false;
             // 
@@ -262,6 +270,46 @@ namespace BoozewasherApp.Forms.EmployeeForms
             this.pictureBox7.Size = new System.Drawing.Size(66, 55);
             this.pictureBox7.TabIndex = 14;
             this.pictureBox7.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Gadugi", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(220, 31);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "6";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Gadugi", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 58);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(220, 31);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "6";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Gadugi", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(0, 58);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(220, 31);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "0";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Gadugi", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(3, 58);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(220, 31);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "0";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // EmployeeHomeUserControl
             // 
@@ -317,5 +365,9 @@ namespace BoozewasherApp.Forms.EmployeeForms
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox6;
         private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
     }
 }

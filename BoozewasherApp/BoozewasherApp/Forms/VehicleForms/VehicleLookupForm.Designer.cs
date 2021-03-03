f76b0a1035f1259d6ecc98bee491b390ad7688be
Ravni Arador <ravarador@gmail.com>
2021-03-03 12:11:59 +0800

    Added FilterBy and OrderBy in VehicleLookupForm

--- BoozewasherApp/BoozewasherApp/Forms/VehicleForms/VehicleLookupForm.Designer.cs
+++ BoozewasherApp/BoozewasherApp/Forms/VehicleForms/VehicleLookupForm.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.dgvVehicle = new System.Windows.Forms.DataGridView();
+            this.comboOrderBy = new System.Windows.Forms.ComboBox();
+            this.comboFilterBy = new System.Windows.Forms.ComboBox();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtboxOrderBy = new System.Windows.Forms.TextBox();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtboxFilterBy = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvVehicle)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,11 +58,85 @@
             this.dgvVehicle.TabIndex = 1;
             this.dgvVehicle.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvVehicle_CellDoubleClick);
             // 
+            // comboOrderBy
+            // 
+            this.comboOrderBy.FormattingEnabled = true;
+            this.comboOrderBy.Location = new System.Drawing.Point(80, 59);
+            this.comboOrderBy.Name = "comboOrderBy";
+            this.comboOrderBy.Size = new System.Drawing.Size(161, 21);
+            this.comboOrderBy.TabIndex = 31;
+            // 
+            // comboFilterBy
+            // 
+            this.comboFilterBy.FormattingEnabled = true;
+            this.comboFilterBy.Location = new System.Drawing.Point(80, 33);
+            this.comboFilterBy.Name = "comboFilterBy";
+            this.comboFilterBy.Size = new System.Drawing.Size(161, 21);
+            this.comboFilterBy.TabIndex = 30;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(397, 58);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(101, 23);
+            this.btnSort.TabIndex = 29;
+            this.btnSort.Text = "Sort Vehicles";
+            this.btnSort.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Order by: ";
+            // 
+            // txtboxOrderBy
+            // 
+            this.txtboxOrderBy.Location = new System.Drawing.Point(247, 60);
+            this.txtboxOrderBy.Name = "txtboxOrderBy";
+            this.txtboxOrderBy.Size = new System.Drawing.Size(144, 20);
+            this.txtboxOrderBy.TabIndex = 27;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(397, 31);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(101, 23);
+            this.btnFilter.TabIndex = 26;
+            this.btnFilter.Text = "Filter Vehicles";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Filter By: ";
+            // 
+            // txtboxFilterBy
+            // 
+            this.txtboxFilterBy.Location = new System.Drawing.Point(247, 33);
+            this.txtboxFilterBy.Name = "txtboxFilterBy";
+            this.txtboxFilterBy.Size = new System.Drawing.Size(144, 20);
+            this.txtboxFilterBy.TabIndex = 24;
+            // 
             // VehicleLookupForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(530, 628);
+            this.Controls.Add(this.comboOrderBy);
+            this.Controls.Add(this.comboFilterBy);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtboxOrderBy);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtboxFilterBy);
             this.Controls.Add(this.dgvVehicle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "VehicleLookupForm";
@@ -63,11 +145,20 @@
             this.Load += new System.EventHandler(this.VehicleLookupForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvVehicle)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvVehicle;
+        private System.Windows.Forms.ComboBox comboOrderBy;
+        private System.Windows.Forms.ComboBox comboFilterBy;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtboxOrderBy;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtboxFilterBy;
     }
 }

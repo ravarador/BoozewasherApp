12f78aa776a7fe62ccead60c1259b8e9684d9418
Ravni Arador <ravarador@gmail.com>
2021-03-03 12:10:18 +0800

    Added FilterBy and OrderBy in ServiceLookupForm (UI Only)

--- BoozewasherApp/BoozewasherApp/Forms/ServiceForms/ServiceLookupForm.Designer.cs
+++ BoozewasherApp/BoozewasherApp/Forms/ServiceForms/ServiceLookupForm.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.dgvService = new System.Windows.Forms.DataGridView();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtboxOrderBy = new System.Windows.Forms.TextBox();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtboxFilterBy = new System.Windows.Forms.TextBox();
+            this.comboFilterBy = new System.Windows.Forms.ComboBox();
+            this.comboOrderBy = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvService)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,11 +58,85 @@
             this.dgvService.TabIndex = 0;
             this.dgvService.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvService_CellDoubleClick);
             // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(388, 57);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(101, 23);
+            this.btnSort.TabIndex = 13;
+            this.btnSort.Text = "Sort Services";
+            this.btnSort.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Order by: ";
+            // 
+            // txtboxOrderBy
+            // 
+            this.txtboxOrderBy.Location = new System.Drawing.Point(238, 59);
+            this.txtboxOrderBy.Name = "txtboxOrderBy";
+            this.txtboxOrderBy.Size = new System.Drawing.Size(144, 20);
+            this.txtboxOrderBy.TabIndex = 11;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(388, 30);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(101, 23);
+            this.btnFilter.TabIndex = 10;
+            this.btnFilter.Text = "Filter Services";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Filter By: ";
+            // 
+            // txtboxFilterBy
+            // 
+            this.txtboxFilterBy.Location = new System.Drawing.Point(238, 32);
+            this.txtboxFilterBy.Name = "txtboxFilterBy";
+            this.txtboxFilterBy.Size = new System.Drawing.Size(144, 20);
+            this.txtboxFilterBy.TabIndex = 8;
+            // 
+            // comboFilterBy
+            // 
+            this.comboFilterBy.FormattingEnabled = true;
+            this.comboFilterBy.Location = new System.Drawing.Point(71, 32);
+            this.comboFilterBy.Name = "comboFilterBy";
+            this.comboFilterBy.Size = new System.Drawing.Size(161, 21);
+            this.comboFilterBy.TabIndex = 14;
+            // 
+            // comboOrderBy
+            // 
+            this.comboOrderBy.FormattingEnabled = true;
+            this.comboOrderBy.Location = new System.Drawing.Point(71, 58);
+            this.comboOrderBy.Name = "comboOrderBy";
+            this.comboOrderBy.Size = new System.Drawing.Size(161, 21);
+            this.comboOrderBy.TabIndex = 15;
+            // 
             // ServiceLookupForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(523, 626);
+            this.Controls.Add(this.comboOrderBy);
+            this.Controls.Add(this.comboFilterBy);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtboxOrderBy);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtboxFilterBy);
             this.Controls.Add(this.dgvService);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "ServiceLookupForm";
@@ -63,11 +145,20 @@
             this.Load += new System.EventHandler(this.ServiceLookupForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvService)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvService;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtboxOrderBy;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtboxFilterBy;
+        private System.Windows.Forms.ComboBox comboFilterBy;
+        private System.Windows.Forms.ComboBox comboOrderBy;
     }
 }

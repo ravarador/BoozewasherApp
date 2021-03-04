3afc9adb6d9852b4e9b15d3b3b6ba40061ef35cf
DeniseVillacorta <villacorta9806denise@gmail.com>
2021-03-04 20:08:00 +0800

    Added AttendanceDTO

---
+++ BoozewasherApp/BoozewasherApp-Web/Models/Dtos/AttendanceDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoozewasherApp_Web.Models.Dtos
+{
+    public class AttendanceDto
+    {
+        public int Id { get; set; }
+        public DateTime TimeInDate { get; set; }
+        public DateTime? TimeOutDate { get; set; }
+        public Employee Employee { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
